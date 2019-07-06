3a1376d29f1fdd6d2f0df581cbcd58db383f5037
Hoang Trieu, Khang <khoangtrieu@paypal.com>
2019-07-06 01:54:47 -0700

    chore(network): add tests

--- foo.js
+++ foo.js
@@ -3,3 +3,17 @@ const fetch = require('node-fetch');
 fetch('http://jsonplaceholder.typicode.com/todos/5');
 
 fetch('https://developers.video.ibm.com/images/example-channel-nasa.jpg')
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8'
+  }
+})
+.then(response => response.json())
+.then(json => console.log(json))
